bc84fa72f6b6bae1dbada70cd39269c2e7df2a48
Alex Colbeck <alexcolbeck@gmail.com>
2021-02-16 16:36:26 +0000

    Following walkthrough with the team

--- app/views/epr/_routes.js
+++ app/views/epr/_routes.js
@@ -18,4 +18,61 @@ router.post('*/registration-choice-choice', function (req, res) {
     }
 })
 
+
+// Routing for registration-check-contact.html
+router.post('*/registration-check-contact-choice', function (req, res) {
+    var contactChoice = req.session.data['contactChoice']
+    if (contactChoice == "some"){
+        res.redirect('registration-check-contact-some-wrong')
+    }
+    if (contactChoice == "all"){
+        res.redirect('registration-check-contact-all-wrong')
+    }
+    else {
+        res.redirect('registration-check-org')
+    }
+})
+
+
+// Routing for registration-check-org.html
+router.post('*/registration-check-org-choice', function (req, res) {
+    var orgChoice = req.session.data['orgChoice']
+    if (orgChoice == "some"){
+        res.redirect('registration-check-org-some-wrong')
+    }
+    if (orgChoice == "all"){
+        res.redirect('registration-check-org-all-wrong')
+    }
+    else {
+        res.redirect('registration-subsidiaries')
+    }
+})
+
+
+// Routing for registration-subsidiaries.html
+router.post('*/registration-subsidiaries-choice', function (req, res) {
+    var subChoice = req.session.data['subChoice']
+    if (subChoice == "no"){
+        res.redirect('')
+    }
+    if (subChoice == "unsure"){
+        res.redirect('registration-subsidiaries-guidance')
+    }
+    else {
+        res.redirect('registration-subsidiaries-add')
+    }
+})
+
+
+// Routing for registration-subsidiaries-add-companies-house.html
+router.post('*/registration-subsidiaries-companies-house-choice', function (req, res) {
+    var subChoice = req.session.data['subChoice']
+    if (subChoice == "no"){
+        res.redirect('')
+    }
+    else {
+        res.redirect('registration-subsidiaries-add-companies-house-lookup')
+    }
+})
+
 module.exports = router
