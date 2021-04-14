60e8fff11804fb29a038dff8e6933b9b299c7b6f
Alex Colbeck <alexcolbeck@gmail.com>
2021-04-14 09:42:46 +0100

    Altering the check contact details step

--- app/routes.js
+++ app/routes.js
@@ -22,6 +22,7 @@ router.use('/v1/defra-id', require('./views/v1/defra-id/_routes'));
 
 router.use('/v2/epr', require('./views/v2/epr/_routes'));
 router.use('/v2/defra-id', require('./views/v2/defra-id/_routes'));
+router.use('/v2/defra-id-manage', require('./views/v2/defra-id-manage/_routes'));
 router.use('/v2/drs', require('./views/v2/drs/_routes'));
 router.use('/v2/epr-obligation-checker', require('./views/v2/epr-obligation-checker/_routes'));
 
