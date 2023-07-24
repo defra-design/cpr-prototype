699e2b520af190951bb1160bfba8c843a1067568
j-goodwin-kainos <136819261+j-goodwin-kainos@users.noreply.github.com>
2023-07-24 10:55:08 +0000

    Contact registration journey updated. Only custom address missing

--- app/views/paper-cups/registration/version-1/_routes.js
+++ app/views/paper-cups/registration/version-1/_routes.js
@@ -6,9 +6,20 @@ router.post('*/contact-address-question', function (req, res) {
     var addressOther = req.session.data['address-type'];
     
     if (addressOther === "other") {
-        res.redirect('../postcode-lookup');
+        res.redirect('./postcode-lookup');
     } else {
         res.redirect('./contact-telephone');
+    }
+});
+
+router.post('*/contact-person-question', function (req, res) {
+    var contactOther = req.session.data['contact-name'];
+
+    if (contactOther === "other") {
+        res.redirect('./contact-full-name');
+    } else {
+        req.session.data['contact-full-name'] = contactOther;
+        res.redirect('./contact-preference?other-contact=no');
     }
 });
 
