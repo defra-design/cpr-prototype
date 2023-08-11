3c92f428ab6f2e07788461d94e4296e0d3778e51
j-goodwin-kainos <136819261+j-goodwin-kainos@users.noreply.github.com>
2023-08-11 12:09:27 +0000

    Payment for cup seller england

--- app/views/paper-cups/sellers-registration/version-1/_routes.js
+++ app/views/paper-cups/sellers-registration/version-1/_routes.js
@@ -65,4 +65,12 @@ router.post('*/address-manual-question', function (req, res) {
     res.redirect('../contact-person');
 });
 
+router.post('*/payment-made-question', function (req, res) {
+    if (req.session.data['payment-made-england'] === "yes") {
+        res.redirect('./payment-confirmation-england');
+    } else {
+        res.redirect('../../registration-task-list');
+    }
+});
+
 module.exports = router;
