8105b029b67548cf58aa8b68d06d7751179cf721
Joe Horton <joeehorton@protonmail.com>
2023-09-20 18:28:32 +0100

    wed pm pm

--- app/assets/javascripts/loading-file-success.js
+++ app/assets/javascripts/loading-file-success.js
@@ -2,5 +2,5 @@ $(document).ready(function () {
     // Handler for .ready() called.
     window.setTimeout(function () {
         location.href = "./check-file.html";
-    }, 3000);
+    }, 5000);
 });
