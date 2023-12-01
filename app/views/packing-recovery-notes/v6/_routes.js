92a97b20c73834b7416265cc4252c2f37b8f4c05
ludwig grant <ludwig.grant@digital.hmrc.gov.uk>
2023-12-01 15:53:37 +0000

    new version

---
+++ app/views/packing-recovery-notes/v6/_routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+// Add your routes here - above the module.exports line
+
+
+
+// GENERIC NEXT PAGE ELEMENT
+router.post('*', function (req, res, next) {
+  console.log(req.body);
+
+  if (req.body['next-page']) {
+    res.redirect(req.body['next-page']);
+  } else {
+    next();
+  }
+});
+
+
+module.exports = router
