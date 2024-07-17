faf72babb5f5916820e7c362fb0d9a82233ecae6
that-firefly <54668579+that-firefly@users.noreply.github.com>
2024-07-17 11:50:49 +0100

    Adding folders and pages
    Adding folders and pages to small producers

---
+++ app/views/small-producers/account-creation/v2/_routes.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const router = express.Router()
+
+// Routing for Page
+router.post('*/registered-trade-in-uk', function (req, res) {
+    var registeredUK = req.session.data['registered-in-uk']
+    
+    if (registeredUK == "yes"){
+        res.redirect('type-of-organisation')
+    }
+    else if (registeredUK == "no"){
+        res.redirect('../not-affected/non-uk-organisation')
+    }
+})
+
+// Routing for Page
+router.post('*/organisation-type', function (req, res) {
+    var organisationType = req.session.data['form-organisation-type']
+    
+    if (organisationType == "Limited Company"){
+        res.redirect('../limited-company/about-your-organisation/companies-house-number')
+    }
+    else if (organisationType == "Sole trader"){
+        res.redirect('../sole-trader/about-your-business/trading-name')
+    }
+    else if (organisationType == "Charity"){
+        res.redirect('../not-affected/charity')
+    }
+})
+
+// Routing for Page
+router.post('*/authorised-person', function (req, res) {
+    var authorisedPerson = req.session.data['form-role-in-organisation']
+    
+    if (authorisedPerson == "none-of-the-above") {
+        res.redirect('../authorised-details-on-behalf/consent-from-authorised-person')
+    }
+    else {
+        res.redirect('full-name')
+    }
+})
+
+
+
+
+module.exports = router
