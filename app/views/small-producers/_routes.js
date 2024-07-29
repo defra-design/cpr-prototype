7a0208d5009f7b0527f139515fdc163362e5c93f
that-firefly <54668579+that-firefly@users.noreply.github.com>
2024-07-29 16:21:47 +0100

    Update _routes.js

--- app/views/small-producers/_routes.js
+++ app/views/small-producers/_routes.js
@@ -3,9 +3,10 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 
+
 // Routing for producer status -----> https://defra-cpr-prototype.herokuapp.com/small-producers/self-declare-2-status <------
 router.post('*/self-declare-2-status', function (req, res) {
-    var actingAs = req.session.data['status']
+    var userGroup = req.session.data['status']
     
     if (actingAs == "small-producer"){
         res.redirect('self-declare-3-warning')
@@ -17,6 +18,7 @@ router.post('*/self-declare-2-status', function (req, res) {
         res.redirect('self-declare-4-obligation-checker')
     }  
 })
+
 
 
 
