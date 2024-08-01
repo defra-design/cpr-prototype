dee5c298aff69d33c8c6beb8be74dc98b9b23321
that-firefly <54668579+that-firefly@users.noreply.github.com>
2024-08-01 12:04:32 +0100

    Links
    links on routes

--- app/views/small-producers/links-routes/_routes.js
+++ app/views/small-producers/links-routes/_routes.js
@@ -3,20 +3,16 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 // Routing for self-declare-2-status.html
-router.post('*/self-declare-2-status', function (req, res) {
-    var regChoice = req.session.data['sizeorganisation']
-    if (regChoice == "sizeorganisation"){
-        res.redirect('self-declare-3-warning')
-    }
-    if (regChoice == "sizeorganisation-2"){
-        res.redirect('self-declare-3-warning')
-    }
-    else {
-        res.redirect('self-declare-4-obligation-checker')
+router.post('/self-declare-2-status', function(request, response) {
+    var sizeorganisation = request.session.data['sizeorganisation']
+    
+    if (sizeorganisation === "largeproducers"){
+        response.redirect("self-declare-3-warning")
+    } else if (sizeorganisation === "smallproducers"){
+        response.redirect("self-declare-3-warning")
+    } else {
+        response.redirect("self-declare-4-obligation-checker")
     }
 })
-
-
-
 
 module.exports = router
