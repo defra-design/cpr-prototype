4841b549847f1ca95ab8e81f648d78b3afede3e5
that-firefly <54668579+that-firefly@users.noreply.github.com>
2024-08-01 18:15:39 +0100

    Redirection
    Redirection

--- app/views/small-producers/links-routes/_routes.js
+++ app/views/small-producers/links-routes/_routes.js
@@ -3,19 +3,19 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 // Routing for self-declare-2-status.html
-router.get('/self-declare-2-status', function (req, res) {
+router.get('self-declare-2-status', function (req, res) {
     res.render('self-declare-2-status', {})
 })
 
-router.post('/self-declare-2-status', function (req, res) {
+router.post('self-declare-2-status', function (req, res) {
     // Retrieve the value of sizeorganisation from session data
-    const sizeorganisation = req.session.data['sizeorganisation']
+    var sizeorganisation = req.session.data['sizeorganisation']
 
     // Check the value and redirect accordingly
     if (sizeorganisation === 'largeproducer' || sizeorganisation === 'smallproducer') {
-        res.redirect('/self-declare-3-warning')
+        res.redirect('self-declare-3-warning')
     } else {
-        res.redirect('/self-declare-4-obligation-checker')
+        res.post('self-declare-4-obligation-checker')
     }
 })
 
