c58cf30a460565864d7c43965e2ab9e8545c3d43
that-firefly <54668579+that-firefly@users.noreply.github.com>
2024-08-01 15:34:12 +0100

    Updating
    Updating pages

--- app/views/small-producers/links-routes/_routes.js
+++ app/views/small-producers/links-routes/_routes.js
@@ -2,16 +2,23 @@ const express = require('express')
 const router = express.Router()
 // Add your routes here - above the module.exports line
 
+
 // Routing for self-declare-2-status.html
-router.post('/self-declare-2-status', function(request, response) {
-    var sizeorganisation = request.session.data['sizeorganisation']
-    
-    if (sizeorganisation === "largeproducers"){
-        response.redirect("self-declare-3-warning")
-    } else if (sizeorganisation === "smallproducers"){
-        response.redirect("self-declare-3-warning")
+router.get('self-declare-2-status', function (req, res) {
+    res.render('self-declare-2-status', {});
+})
+
+router.post('self-declare-3-warning', function (req, res) {
+    var sizeorganisation = req.body.sizeorganisation;
+    req.session.data = req.session.data || {};
+    req.session.data['sizeorganisation'] = sizeorganisation;
+
+    if (sizeorganisation === "largeproducer" || sizeorganisation === "smallproducer") {
+        // Block of code to be executed if the condition is true
+        res.redirect('self-declare-3-warning');
     } else {
-        response.redirect("self-declare-4-obligation-checker")
+        // Block of code to be executed if the condition is false
+        res.redirect('self-declare-4-obligation-checker');
     }
 })
 
