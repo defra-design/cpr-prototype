c05fd2d2d26d4169389bd521a69aebfffda94ea5
that-firefly <54668579+that-firefly@users.noreply.github.com>
2024-08-01 16:29:06 +0100

    Fixing margins
    Fixing margins on page

--- app/views/small-producers/links-routes/_routes.js
+++ app/views/small-producers/links-routes/_routes.js
@@ -2,24 +2,22 @@ const express = require('express')
 const router = express.Router()
 // Add your routes here - above the module.exports line
 
-
 // Routing for self-declare-2-status.html
-router.get('self-declare-2-status', function (req, res) {
-    res.render('self-declare-2-status', {});
+router.get('/self-declare-2-status', function (req, res) {
+    res.render('self-declare-2-status', {})
 })
 
-router.post('self-declare-3-warning', function (req, res) {
-    var sizeorganisation = req.body.sizeorganisation;
-    req.session.data = req.session.data || {};
-    req.session.data['sizeorganisation'] = sizeorganisation;
+router.post('/self-declare-2-status', function (req, res) {
+    // Retrieve the value of sizeorganisation from session data
+    const sizeorganisation = req.session.data['sizeorganisation']
 
-    if (sizeorganisation === "largeproducer" || sizeorganisation === "smallproducer") {
-        // Block of code to be executed if the condition is true
-        res.redirect('self-declare-3-warning');
+    // Check the value and redirect accordingly
+    if (sizeorganisation === 'largeproducer' || sizeorganisation === 'smallproducer') {
+        res.redirect('/self-declare-3-warning')
     } else {
-        // Block of code to be executed if the condition is false
-        res.redirect('self-declare-4-obligation-checker');
+        res.redirect('/self-declare-4-obligation-checker')
     }
 })
+
 
 module.exports = router
