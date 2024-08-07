521e283e04773e6f8cf64b0f318835d3d0813750
that-firefly <54668579+that-firefly@users.noreply.github.com>
2024-08-07 12:33:37 +0100

    Updating copy
    Updating copy in pages

--- app/views/small-producers/_routes.js
+++ app/views/small-producers/_routes.js
@@ -3,34 +3,19 @@ const router = express.Router()
 
 
 // Routing for Page /small-producers/self-declare-2-status
-router.post('*/task-list-2', function (req, res) {
+router.post('*/task-list-1', function (req, res) {
     var sizeorganisation = req.session.data['sizeorganisation'];  
-    if (sizeorganisation === "largeproducer") {
+    if (sizeorganisation === "smallproducer") {
         // Handle smallproducer case
-        res.redirect('./task-list-2-large-producer');
-    } else if (sizeorganisation === "smallproducer") {
+        res.redirect('./task-list-1');
+    } else if (sizeorganisation === "largeproducer") {
         // Handle largeproducer case
-        res.redirect('./task-list-2-small-producer');
+        res.redirect('./task-list-1');
     } else {
         var dontknown = req.session.data['dontknow'];
         res.redirect('https://defra-cpr-prototype.herokuapp.com/obligation-checker/v5/start-page');
     }
 });
-
-
-
-// Routing for Page /small-producers/self-declare-2-status
-router.post('*/selfdeclarenext', function (req, res) {
-    var declarationanswer = req.session.data['declarationanswer'];  
-    if (declarationanswer === "yes") {
-        // Handle case where the answer is "yes"
-        res.redirect('./self-declare-5-success');
-    } else if (declarationanswer === "no") {
-        // Handle case where the answer is "no"
-        res.redirect('./no-page');
-    }
-});
-
 
 // Routing for Page /small-producers/self-declare-3-warning-2
 router.post('*/selfdeclarenext', function (req, res) {
