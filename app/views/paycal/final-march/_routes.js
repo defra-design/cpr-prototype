b5376ca17c808598fa83bad685ccf5e091de02bf
GBell-GH <graham.bell@eviden.com>
2025-02-24 13:22:39 +0000

    PayCal content update
    Changes to dashboad and classification procedure

--- app/views/paycal/final-march/_routes.js
+++ app/views/paycal/final-march/_routes.js
@@ -8,22 +8,25 @@ router.post('*/classification', function (req, res) {
     var calculation = req.session.data['calcRun']
     
     if (calculation == "outputDelete"){
-        res.redirect('output-1')
+        res.redirect('output-4')
     }
     else if (calculation == "outputPlay"){
         res.redirect('output-2')
     }
-    else if (calculation == "outputInitialRun"){
+    else if (calculation == "outputClassify"){
         res.redirect('output-3')
     }
-    else if (calculation == "outputInterimRecalculationRun"){
-        res.redirect('output-4')
+    else if (calculation == "outputInitialRun"){
+        res.redirect('output-1')
     }
-    else if (calculation == "outputFinalRecalculationRun"){
+    else if (calculation == "outputInterimRecalculationRun"){
         res.redirect('output-5')
     }
-    else if (calculation == "outputFinalRun"){
+    else if (calculation == "outputFinalRecalculationRun"){
         res.redirect('output-6')
+    }
+    else if (calculation == "outputFinalRun"){
+        res.redirect('output-7')
     }
 })
 
