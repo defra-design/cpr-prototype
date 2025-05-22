dfc7159b1a7a1519c9e98d366ef214b599e91b9d
Dinesh-9829 <dinesh.jayachandran.external@eviden.com>
2025-05-22 21:57:14 +0530

    'v7_update'

--- app/routes.js
+++ app/routes.js
@@ -106,6 +106,7 @@ router.use('/packing-recovery-notes/v6-4', require('./views/packing-recovery-not
 router.use('/packing-recovery-notes/v6-5', require('./views/packing-recovery-notes/v6/_routes'));
 router.use('/packing-recovery-notes/v6-6-rexp', require('./views/packing-recovery-notes/v6/_routes'));
 router.use('/packing-recovery-notes/account-creation', require('./views/packing-recovery-notes/account-creation/build/_routes'));
+router.use('/packing-recovery-notes/v7', require('./views/packing-recovery-notes/v7/_routes'));
 
 
 //PayCal
